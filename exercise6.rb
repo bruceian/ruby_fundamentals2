7cedf90bec66a3045a171e75a603bbfb46196c60
Ian Bruce <brucegordonian@gmail.com>
2016-08-10 16:55:49 -0400

    Added rice and added spaces between each printing of the list exit git status

--- exercise6.rb
+++ exercise6.rb
@@ -4,6 +4,13 @@ def show_list(list)
   list.sort.map do |item|
     puts "* #{item}"
   end
+  puts " "
 end
 
+show_list(grocery_list)
+
+grocery_list << "rice"
+show_list(grocery_list)
+
+grocery_list << "salmon" # I forgot salmon as well
 show_list(grocery_list)
