9b4135f318504b0c8650be90ea5fc4aac7d21cee
Ian Bruce <brucegordonian@gmail.com>
2016-08-10 20:19:23 -0400

    Deleted salmon and accessed the second string in the array using its index.

--- exercise6.rb
+++ exercise6.rb
@@ -10,3 +10,17 @@ end
 grocery_list << "rice"
 grocery_list << "salmon" # I forgot salmon as well
 show_list(grocery_list)
+
+puts "There are #{grocery_list.count} items on my list."
+
+if grocery_list.include?("bananas")
+  puts "You don't need bananas homie, you good."
+else
+  puts "YOU NEED FUCKIN BANANAS DOG."
+end
+
+puts grocery_list[1]  # accesses tissues from original array (unmodified?)
+
+grocery_list.delete("salmon")
+
+show_list(grocery_list)   # removed salmon list
